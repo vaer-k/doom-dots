433e01ebca21c8507b8a3c0e0ea76df153b1b8c7
Vincent Raerek <vincent@monadnomad.dev>
2020-02-22 13:05:28 -0800

    Remove dead config

--- config.el
+++ config.el
@@ -51,29 +51,8 @@
 
 
 ;;;;
-;; Elixir
-;;
-
-;; Create a buffer-local hook to run elixir-format on save, only when we enable elixir-mode. Will not source a `.formatter.exs`. See for details: https://github.com/elixir-editors/emacs-elixir#elixir-format
-;;(add-hook 'elixir-mode-hook
-;;          (lambda () (add-hook 'before-save-hook 'elixir-format nil t)))
-
-;;(defun my-web-mode-hook ()
-;;  "Hooks for Web mode."
-;;    (setq web-mode-markup-indent-offset 2)
-;;    (setq web-mode-css-indent-offset 2)
-;;    (setq web-mode-code-indent-offset 2)
-;;    (setq web-mode-indent-style 2)
-;;)
-;;(add-hook 'web-mode-hook  'my-web-mode-hook)
-;;
-;;(add-to-list 'auto-mode-alist '("\\.eex\\'" . web-mode))
-
-;;;;
 ;; Clojure
 ;;
-;; (global-set-key (kbd "C-)") 'sp-forward-slurp-sexp)
-;; (global-set-key (kbd "C-(") 'sp-forward-barf-sexp)
 (add-hook 'clojure-mode-hook #'enable-paredit-mode)
 
 ;;;;
